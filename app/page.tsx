32c817a9f3a3bfd301be60d875b35e514d4b1fa5
Saoa35 <saoamah@gmail.com>
2023-10-15 10:02:23 +0400

    + added UserButton comp.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
+      <UserButton afterSignOutUrl="/" />
       <h2>HOME PAGE</h2>
     </div>
   );
