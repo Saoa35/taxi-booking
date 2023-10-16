96532a4b24723a4eb34d514fd296cd822cf21671
Saoa35 <saoamah@gmail.com>
2023-10-16 11:38:14 +0400

    + created AutocompleteAddress component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import { Booking } from "@/components/Booking/Booking";
+
 export default function Home() {
   return (
     <div>
       <div className=" grid grid-cols-1 sm:grid-cols-3">
-        <div className=" bg-blue-100">Booking</div>
+        <div className=" bg-blue-100">
+          <Booking />
+        </div>
         <div className="col-span-2 bg-red-100">Map</div>
       </div>
     </div>
