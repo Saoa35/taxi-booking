87ba412d24bba999cff018c48f6808bb3ebf744d
Saoa35 <saoamah@gmail.com>
2023-10-16 11:17:37 +0400

    * edited NavBar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,6 @@
-import { UserButton } from "@clerk/nextjs";
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div>
-      <UserButton afterSignOutUrl="/" />
       <h2>HOME PAGE</h2>
     </div>
   );
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,11 +1,25 @@
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export const NavBar = () => {
   return (
-    <div>
-      <div>
+    <div className="flex justify-between p-3 px-10 border-b-[1px]">
+      <div className="flex gap-10 items-center">
         <Image src="/logo.png" alt="logo" width={120} height={60} />
+
+        <div className="sm:flex gap-6 hidden">
+          <h2 className=" hover:bg-gray-100 p-2 rounded-md cursor-pointer transition-all">
+            Home
+          </h2>
+          <h2 className=" hover:bg-gray-100 p-2 rounded-md cursor-pointer transition-all">
+            History
+          </h2>
+          <h2 className=" hover:bg-gray-100 p-2 rounded-md cursor-pointer transition-all">
+            Help
+          </h2>
+        </div>
       </div>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 };
