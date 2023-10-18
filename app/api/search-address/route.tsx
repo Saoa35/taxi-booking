58ab1ff81d45fc02afc84117b48963d3b422e3e2
Saoa35 <saoamah@gmail.com>
2023-10-18 10:06:10 +0400

    + added searchText var

--- app/api/search-address/route.tsx
+++ app/api/search-address/route.tsx
@@ -1,5 +1,9 @@
 import { NextResponse } from "next/server";
 
 export async function GET(request: any) {
+  const { searchParams } = new URL(request.url);
+
+  const searchText = searchParams.get("q");
+
   return NextResponse.json({ data: "Test" });
 }
