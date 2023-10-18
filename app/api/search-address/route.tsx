7d0f7ddbcd0859a98316d21b80def4fa16a59c87
Saoa35 <saoamah@gmail.com>
2023-10-18 10:40:44 +0400

    + completed route.tsx

--- app/api/search-address/route.tsx
+++ app/api/search-address/route.tsx
@@ -1,9 +1,27 @@
 import { NextResponse } from "next/server";
 
+const BASE_URL = "https://api.mapbox.com/search/searchbox/v1/suggest";
+
 export async function GET(request: any) {
   const { searchParams } = new URL(request.url);
 
   const searchText = searchParams.get("q");
 
-  return NextResponse.json({ data: "Test" });
+  const res = await fetch(
+    BASE_URL +
+      "?q=" +
+      searchText +
+      "?language=en&limit=6&session_token=0f3e34ee-3cbb-4441-88b4-17316b502255&country=US" +
+      "&access_token=" +
+      process.env.MAPBOX_ACCESS_TOKEN,
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  const searchResult = await res.json();
+
+  return NextResponse.json(searchResult);
 }
